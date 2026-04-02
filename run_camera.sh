808b00756b06fc4c4c951f8a0ae78805edca1226
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2026-04-01 21:00:46 -0700

    Update run_camera.sh

--- run_camera.sh
+++ run_camera.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/dcabahug1/Github/rpi-security-camera
+cd /home/dcabahug1/Github/rpi-security-camera || exit 1
 source .venv/bin/activate
+python -m pip install -r requirements.txt
 python opencv-practice.py
