383fc83f27960af6fb5a87d2f25f59a8eb60be8c
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2026-04-01 19:58:20 -0700

    learn camera inputs and processing

---
+++ opencv-practice.py
@@ -0,0 +1,29 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+  ret, frame = cap.read()
+
+  if not ret:
+    print("Unable to read frame")
+    break
+
+  cv2.rectangle(frame, (20,20), (80,80), (255,0,0), 2)
+
+  gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+  cv2.imshow("Webcam", frame)
+  cv2.imshow('Webcame_Gray', gray_frame)
+
+  key = cv2.waitKey(1) & 0xFF
+
+  if key == ord('q'):
+    break
+  elif key == ord('s'):
+    cv2.imwrite('color_snapshot.jpg', frame)
+  elif key == ord('g'):
+    cv2.imwrite('gray_snapshot.jpg', gray_frame)
+
+cap.release()
+cv2.destroyAllWindows()
