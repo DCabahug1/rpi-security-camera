be55ffe7aca30b176d4c13fd681b5450ff99dfbe
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2026-04-01 20:11:25 -0700

    Update opencv-practice.py

--- opencv-practice.py
+++ opencv-practice.py
@@ -2,6 +2,12 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+cv2.namedWindow("Webcam", cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty("Webcam", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+cv2.namedWindow("Webcam_Gray", cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty("Webcam_Gray", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 while True:
   ret, frame = cap.read()
 
@@ -14,7 +20,7 @@ while True:
   gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
   cv2.imshow("Webcam", frame)
-  cv2.imshow('Webcame_Gray', gray_frame)
+  cv2.imshow('Webcam_Gray', gray_frame)
 
   key = cv2.waitKey(1) & 0xFF
 
